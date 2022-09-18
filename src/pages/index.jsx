7a73cc13c61b1b89d1c4a025424ff67d5a1f0c61
DishantWakalkar <dishantwakalkar@gmail.com>
2022-09-18 22:22:35 +0530

    First Commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+export { default as Overall } from './Overall';
+export { default as ColorPicker } from './ColorPicker';
+export { default as Area } from './Charts/Area';
+export { default as Bar } from './Charts/Bar';
+export { default as ColorMapping } from './Charts/ColorMapping';
+export { default as Line } from './Charts/Line';
+export { default as Pie } from './Charts/Pie';
+export { default as Pyramid } from './Charts/Pyramid';
+export { default as Stacked } from './Charts/Stacked';
